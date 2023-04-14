6fb4d30a4149eb45870e509243edad60057eaaa2
Alexandra Khoo <thedatacurious@gmail.com>
2023-04-14 20:13:31 +0800

    Scrolly for atlas of youtube

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,92 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Scroller from "@sveltejs/svelte-scroller";
+  import Map from "./lib/Map.svelte";
+  let index, indexPrev, src, alt, offset, progress;
+
+  const mapActions = [
+    ["/mapLegend.jpg", "Legend showing terrain mapping to emotion"],
+    [
+      "/plantLegend.jpg",
+      "Legend showing how the plant height refers to time spent watching and the number of leaves refers to count of videos",
+    ],
+    [
+      "/strokeLegend.jpg",
+      "Legend on the video format. A stroke refers to competition streaming; a filled block refers to a documentary or video essay; a circle refers to a fictional clip",
+    ],
+    [
+      "/colourLegend.jpg",
+      "Legend on the video content. Green refers to nature; blue refers to climbing; yellow refers to current affairs; pink refers to comedy",
+    ],
+  ];
+
+  // Reactive code to trigger MAP actions
+  $: if (index != indexPrev) {
+    if (mapActions[+index]) {
+      src = mapActions[index][0];
+      alt = mapActions[index][1];
+    }
+    indexPrev = index;
+  }
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+    <h1>Atlas of my Escapism</h1>
+    <p>For 60 days, I tracked my Youtube habits.</p>
+    <p>
+      Depending on the day, I may be in a different headspace when watching
+      Youtube.
+    </p>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <Scroller top={0.2} bottom={0.8} bind:index bind:offset bind:progress>
+    <div slot="background">
+      <img {src} {alt} />
+    </div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div slot="foreground">
+      <section />
+      <section>
+        <p>Each lump with prints represents a day of watching.</p>
+        <p>There were four days where I didn't watch any Youtube.</p>
+      </section>
+      <section>
+        <p>I'm a climber...</p>
+        <p>so it's probably not that surprising</p>
+        <p>that spikes in my watch time were due to climbing competitions.</p>
+        <p>The max time was almost 5 hours</p>
+      </section>
+      <section>
+        <p>
+          I'm also a fan of nature and comedy videos—and the occasional ones on
+          current affairs
+        </p>
+        <p>Time to explore the map on your own!</p>
+      </section>
+    </div>
+  </Scroller>
+
+  <div class="full-screen-width"><Map /></div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  img {
+    width: 100%;
+    height: auto;
+    margin: 0.5em 0 1em 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  section {
+    height: 80vh;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .full-screen-width {
+    position: relative;
+    left: 50%;
+    right: 50%;
+    margin-left: -50vw;
+    margin-right: -50vw;
+    max-width: 100vw;
+    width: 100vw;
   }
 </style>
---
+++ src/lib/Map.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import OpenSeadragon from "openseadragon";
+
+  let viewer;
+
+  onMount(() => {
+    viewer = OpenSeadragon({
+      id: "mapContainer",
+      prefixUrl: "../src/data/atlas_files/",
+      tileSources: "../src/data/atlas.dzi",
+      maxZoomLevel: 8,
+    });
+  });
+
+  onDestroy(() => {
+    viewer.remove();
+  });
+</script>
+
+<div id="mapContainer" />
+
+<style>
+  #mapContainer {
+    position: relative;
+    width: 100%;
+    height: 80vh;
+  }
+</style>
